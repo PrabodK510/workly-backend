8415816526501a06be0a56d34893c441573a4749
PrabodK510 <prabodkaushalya347@gmail.com>
2026-04-07 14:57:55 +0530

    Add files via upload

--- models/Category.js
+++ models/Category.js
@@ -5,15 +5,18 @@ const categorySchema = new mongoose.Schema({
   plans: {
     starter: {
       price: { type: Number, default: 0 },
-      commission: { type: Number, default: 15 } // percentage
+      commission: { type: Number, default: 15 }, // percentage
+      description: { type: String, default: '' }
     },
     premium: {
       price: { type: Number, default: 0 },
-      commission: { type: Number, default: 15 }
+      commission: { type: Number, default: 15 },
+      description: { type: String, default: '' }
     },
     enterprise: {
       price: { type: Number, default: 0 },
-      commission: { type: Number, default: 15 }
+      commission: { type: Number, default: 15 },
+      description: { type: String, default: '' }
     }
   },
   iconName: { type: String, default: 'tool' },
